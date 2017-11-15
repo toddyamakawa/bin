22714e9bb6f546ab38b7b9bf2832b8745d9abeaf
Todd Yamakawa <todd.yamakawa@arm.com>
2017-11-14 21:56:40 -0600

    bin: Creating bin plugin in bin directory.

---
+++ bin.plugin.zsh
@@ -0,0 +1,20 @@
+
+# --- ack ---
+if [[ -f ~/bin/ack ]]; then
+	alias acki='ack -i'
+fi
+
+
+# --- fzf ---
+if [[ -f ~/.fzf.zsh ]]; then
+	source ~/.fzf.zsh
+	alias vif='vi $(fzf)'
+fi
+
+
+# --- h ---
+if [[ -f ~/bin/h.sh ]]; then
+	source $(readlink -f ~/bin/h.sh)
+	alias hlog='h -i error "info|success\w+" "warn\w+|exit\w+"'
+fi
+
