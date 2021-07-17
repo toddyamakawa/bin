555da102f35fe7513a9b9a9fe8170302c7bf0291
Todd Yamakawa <todd.yamakawa@arm.com>
2021-07-17 16:18:05 -0500

    autcd.zsh: Cleaning up script.

--- sourceme/autocd.zsh
+++ sourceme/autocd.zsh
@@ -1,13 +1,16 @@
 #!/usr/bin/env zsh
 # Write to $zshcd to cd to it
 
+# Check if $zshcd already exists
+[[ -r "$zshcd" ]] && return 0
+
 # Create $zshcd file
-zshcd="/tmp/zsh-$(id -u).$$.path"
+declare -r zshcd="/tmp/zsh-$(id -u).$$.path"
 if [[ ! -r "$zshcd" ]]; then
 	(mkfifo "$zshcd")
-	touch $zshcd
+	touch "$zshcd"
 fi
-chmod 600 $zshcd
+chmod 600 "$zshcd"
 
 # Create $zshpath file
 zshpath="$(mktemp --suffix=.zsh-path)"
@@ -16,20 +19,24 @@ zshpath="$(mktemp --suffix=.zsh-path)"
 {
 	while read line; do
 		if [[ -d "$line" ]]; then
-			echo "$line" > $zshpath
+			echo "$line" > "$zshpath"
 			kill -s USR1 $$
 		fi
-
-	done < <(tail -f $zshcd)
+	done < <(tail -f "$zshcd")
 } &
 
 # Trap all USR1 signals
 TRAPUSR1() {
-	if [[ -f "$zshpath" ]]; then
-		newpath="$(cat $zshpath)"
-		if [[ -d "$newpath" && "$newpath" != "$(pwd)" && -o zle ]]; then
-			zle -R && cd "$newpath" && precmd && zle reset-prompt 2>/dev/null
-		fi
+	local newpath
+	[[ -f "$zshpath" ]] || return
+	newpath="$(cat $zshpath)"
+	[[ -d "$newpath" && "$newpath" != "$(pwd)" ]] || return
+	if [[ -o zle ]]; then
+		#zle -R && cd "$newpath" && precmd 2>/dev/null && zle reset-prompt 2>/dev/null
+		zle -R
+		cd "$newpath"
+		precmd 2>/dev/null
+		zle reset-prompt 2>/dev/null
 	fi
 }
 
