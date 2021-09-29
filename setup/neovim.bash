79efd4ecd2530a159a5ab7d9311e925e7d2df01f
Todd Yamakawa <toddy@google.com>
2021-09-29 21:52:18 +0000

    setup/neovim.bash: Updating.

--- setup/neovim.bash
+++ setup/neovim.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-which brew || exit $?
-# https://dev.to/craftzdog/how-to-install-neovim-on-apple-silicon-m1-mac-27ke
-xcode-select --install
-brew install --HEAD tree-sitter
-brew install --HEAD luajit
-brew install --HEAD neovim
+if (which brew); then
+	# https://dev.to/craftzdog/how-to-install-neovim-on-apple-silicon-m1-mac-27ke
+	xcode-select --install
+	brew install --HEAD tree-sitter
+	brew install --HEAD luajit
+	brew install --HEAD neovim
+else
+	sudo apt -y install neovim
+fi
