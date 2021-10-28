08f1ac82a13260536bcabe838c289b6959445358
Todd Yamakawa <toddy@google.com>
2021-10-28 20:13:03 +0000

    snippets: Fixing whitespace.

--- snippets/parse_args.bash
+++ snippets/parse_args.bash
@@ -5,7 +5,7 @@ declare -a ARGS
 while [[ $# -ge 1 ]]; do
 	arg="$1" && shift
 	case "$arg" in
-	▸   -*) [[ $1 == ${1#-} ]] && { ARGV["$arg"]="$1"; shift; } || ARGV["$arg"]=1;;
-	▸   *) ARGS=("${ARGS[@]}" "$arg");;
+		-*) [[ $1 == ${1#-} ]] && { ARGV["$arg"]="$1"; shift; } || ARGV["$arg"]=1;;
+		*) ARGS=("${ARGS[@]}" "$arg");;
 	esac
 done
