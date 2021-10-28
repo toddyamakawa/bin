144a14998746e4bf2a6c20b76f8eeec37100be30
Todd Yamakawa <toddy@google.com>
2021-10-28 15:39:21 +0000

    2cfg.py: Merging functions.

--- 2cfg.py
+++ 2cfg.py
@@ -10,15 +10,6 @@ args = parser.parse_args()
 info = yaml.safe_load(args.input)
 #print(yaml.dump(info, indent=2))
 
-def to_cfg_none(info, parent=''):
-    return [f"{parent}=null"]
-
-def to_cfg_int(info, parent=''):
-    return [f"{parent}={info}"]
-
-def to_cfg_str(info, parent=''):
-    return [f"{parent}='{info}'"]
-
 def to_cfg_list(info, parent=''):
     config = []
     for index, value in enumerate(info):
@@ -34,11 +25,11 @@ def to_cfg_dict(info, parent=''):
 
 def to_cfg(info, parent = ''):
     if(info == None):
-        return to_cfg_str(info, parent)
+        return [f"{parent}="]
     elif(type(info) == int):
-        return to_cfg_int(info, parent)
+        return [f"{parent}={info}"]
     elif(type(info) == str):
-        return to_cfg_str(info, parent)
+        return [f"{parent}='{info}'"]
     elif(type(info) == list):
         return to_cfg_list(info, parent)
     elif(type(info) == dict):
