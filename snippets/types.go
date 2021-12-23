3f012e1f9478092f5bfb0b43a179e6c37a532b11
Todd Yamakawa <toddy@google.com>
2021-12-23 21:34:57 +0000

    snippets/types.go: Updating.

--- snippets/types.go
+++ snippets/types.go
@@ -10,11 +10,11 @@ func main() {
 	b1, b2 := true, false
 	fmt.Printf("- %v (%T), %v (%T)\n" , b1, b1, b2, b2)
 
-	s := "\"string\""
-	fmt.Printf("- %v (%T)\n", s, s)
+	s := "string"
+	fmt.Printf("- %q (%T)\n", s, s)
 
-	i := 1 << 63 - 1
 	var u uint = 1 << 64 - 1
+	i := int(u)
 	fmt.Printf("- %v (%T), %v (%T)\n", i, i, u, u)
 	fmt.Println("- int  int8  int16  int32  int64")
 	fmt.Println("- uint uint8 uint16 uint32 uint64 uintptr")
