a50413025030e95d9dd0488147a6113829d67de4
Todd Yamakawa <toddy@google.com>
2021-12-28 21:13:47 +0000

    snippets/reader.go: Initial commit.

---
+++ snippets/reader.go
@@ -0,0 +1,28 @@
+//usr/bin/env go run "$0"; exit $?
+// USAGE: reader.go
+// DESCRIPTION: Small Reader example
+
+// https://go.dev/tour/methods/21
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
