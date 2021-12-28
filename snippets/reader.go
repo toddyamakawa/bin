fcaae88eefb0eb96edf7fea85112bcff50db874f
Todd Yamakawa <toddy@google.com>
2021-12-28 21:50:15 +0000

    snippets/reader.go: Adding ROT13 reader.

--- snippets/reader.go
+++ snippets/reader.go
@@ -12,8 +12,30 @@ import (
 	"strings"
 )
 
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r rot13Reader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	for i := 0; i < n; i++ {
+		b := p[i]
+		if b >= 'a' && b <= 'z' {
+			b = ((b - 'a' + 13) % 26) + 'a'
+		} else if b >= 'A' && b <= 'Z' {
+			b = ((b - 'A' + 13) % 26) + 'A'
+		}
+		p[i] = b
+	}
+	return
+}
+
 func main() {
-	r := strings.NewReader("Hello, Reader!")
+
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	// Print directly to stdout
+	//io.Copy(os.Stdout, &r)
 
 	b := make([]byte, 8)
 	for {
