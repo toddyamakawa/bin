36d0913e109cbde2a0798a500e301129bb2a102b
Todd Yamakawa <toddy@google.com>
2022-01-24 14:51:46 -0600

    setup/ssh-keygen.bash: Initial commit.

---
+++ setup/ssh-keygen.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Set permissions
+umask 077
+
+# Check SSH directory
+SSH_DIR="$HOME/.ssh"
+if [[ -d "$SSH_DIR" ]]; then
+	echo -e "\e[31;1mWARNING: '$SSH_DIR' already exists!\e[0m"
+	yesno "Do you wish to continue?" || exit $?
+	echo "Moving '$SSH_DIR' to '$SSH_DIR.backup'..."
+	mv "$SSH_DIR" "$SSH_DIR.backup"
+fi
+mkdir -pv "$SSH_DIR"
+
+# Generate SSH key
+echo "Generating key..."
+ssh-keygen -f "$SSH_DIR/id_rsa" -N ""
+cp $SSH_DIR/id_rsa.pub $SSH_DIR/authorized_keys
+
+# SSH config
+echo "Generating $SSH_DIR/config file..."
+cat >$SSH_DIR/config << SSH_CONFIG
+Host *
+ForwardAgent yes
+ForwardX11 yes
+ServerAliveInterval 120
+StrictHostKeyChecking no
+
+Host ds-gerrit
+	HostName ds-gerrit.euhpc.arm.com
+SSH_CONFIG
+
+# Fix home directory permissions
+chmod +x $HOME
+chmod g-w $HOME
+
+# Hope this works
+echo
+echo 'Testing ssh...'
+echo "$> ssh $(hostname --long) hostname --long"
+if ! (ssh -q -o "BatchMode=yes" -o "ConnectTimeout=3" "$(hostname)" "hostname --long"); then
+	echo 'FAILED!'
+fi
+
