4eeddf9f852c9a98331344a10ac8acf9d07dc939
Todd Yamakawa <toddy@google.com>
2022-01-28 09:17:27 -0600

    *.py: Using python3.

--- 2json.py
+++ 2json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json, yaml
 import argparse, sys
 parser = argparse.ArgumentParser()
--- realdir.py
+++ realdir.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import os.path as path
 print(path.dirname(path.realpath(sys.argv[1])))
