9f7bb2803405dd553c8ab3906665dfd83f187e2f
Todd Yamakawa <toddy@google.com>
2022-01-28 09:05:44 -0600

    setup/spotify.bash: Initial commit.

---
+++ setup/spotify.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# https://www.spotify.com/us/download/linux/
+if (is-debian); then
+	set -ex
+	curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt-get update && sudo apt-get install spotify-client
+fi
+
