dd8d766b8b50cff0b19deace817076a48b4a908d
Todd Yamakawa <toddy@google.com>
2022-03-28 21:26:14 -0500

    setup/qmk.bash: Adding set up.

--- setup/qmk.bash
+++ setup/qmk.bash
@@ -3,5 +3,10 @@
 type python3 || exit $?
 type pip || exit $?
 
-print-run python3 -m pip install --user qmk
+# Install QMK
+print-run python3 -m pip install --user qmk || exit $?
+type qmk || exit $?
+
+# Set up QMK
+qmk setup || exit $?
 
