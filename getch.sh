64f50bce22a567e68827aec048e5d25686d99281
Todd Yamakawa <toddy@google.com>
2023-03-28 04:12:28 -0700

    getch.sh: Adding more keys.

--- getch.sh
+++ getch.sh
@@ -2,24 +2,29 @@
 function getkey() {
 	#read -t 0.5 -srn 1
 	read -srn 1
+	RETVAL=$?
+	echo "$REPLY"
+	return $RETVAL
 }
 
 while true; do
-	getkey
-	key="$REPLY"
+	key="$(getkey)"
 
+	# TODO: Figure out how to handle <ESC> key
 	if [[ "$key" == $'\e' ]]; then
-		getkey
-		key+="$REPLY"
-		getkey
-		key+="$REPLY"
+		key+="$(getkey)"
+		key+="$(getkey)"
 	fi
 
 	case "$key" in
+		# Arrow keys
 		$'\e[A') echo 'up'    ;;
 		$'\e[B') echo 'down'  ;;
 		$'\e[C') echo 'left'  ;;
 		$'\e[D') echo 'right' ;;
+		$'\0a' ) echo 'enter' ;;
+		' '    ) echo 'space' ;;
+		$'\t'  ) echo 'tab'   ;;
 		q) echo "q"; exit ;;
 		*) echo "$key" ;;
 	esac
