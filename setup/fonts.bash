9bf17b1bcc0ab03aca5e2d136728e116aa36e939
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-07-11 16:17:01 -0500

    setup/fonts.bash: Updating.

--- setup/fonts.bash
+++ setup/fonts.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 print-run brew tap homebrew/cask-fonts || exit $?
 print-run brew install --cask font-hack-nerd-font || exit $?
+print-run brew install --cask font-fira-code-nerd-font || exit $?
 
