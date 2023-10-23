e810de166f427a95daa6e694d05b172b4750450c
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-10-23 16:02:00 -0500

    snippets/dotdict.py: Adding another example.

--- snippets/dotdict.py
+++ snippets/dotdict.py
@@ -2,8 +2,19 @@
 # USAGE: ./source.py <FILE>
 # DESCRIPTION: Example for sourcing a file
 
-# https://github.com/nicholasmireles/DotDict
+# https://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
+config = dotdict()
+config.host = 'localhost'
+config.users = dotdict()
+config.users.foo = 'bar'
+print(config)
+
+# https://github.com/nicholasmireles/DotDict
 try:
     from dotdict import DotDict as dd
 except ImportError:
