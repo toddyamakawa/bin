e3731eb77f76cbb3adf496d6a39e41692cd8bef8
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-01-28 16:11:03 -0600

    examples/wrap.c: Minor cleanup.

--- examples/wrap.c
+++ examples/wrap.c
@@ -1,9 +1,9 @@
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
